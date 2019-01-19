501b411540adce463231d89bb882838fdf6e1614
Joshua Scheel <jscheel42@gmail.com>
2019-01-18 18:31:22 -0800

    add git branch master to submodules

--- add.sh
+++ add.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 # $1 = aur package name
+# $2 = branch (default = master)
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+GIT_BRANCH=master
+
 cd $SCRIPT_DIR
-git submodule add https://aur.archlinux.org/${1}.git $1
+git submodule add -b $GIT_BRANCH https://aur.archlinux.org/${1}.git $1
