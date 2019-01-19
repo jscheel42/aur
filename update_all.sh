ccf9a4225607201abe4dcff2998b974482ec9ab2
Joshua Scheel <jscheel42@gmail.com>
2019-01-18 18:19:35 -0800

    simplify v2

--- update_all.sh
+++ update_all.sh
@@ -2,10 +2,6 @@
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-# Git pull this repo & any submodules w/ aur config
-git submodule update --recursive
-# git submodule update --recursive --remote
-
 # Update each package folder
 for d in */; do
     $SCRIPT_DIR/update.sh $d
