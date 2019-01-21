293137c3dad6aa12e9794db9239c55f19be6d8cb
Joshua Scheel <jscheel42@gmail.com>
2019-01-20 17:57:13 -0800

    tor-browser

--- update_all.sh
+++ update_all.sh
@@ -8,7 +8,7 @@ git pull
 # Import GPG Keys
 # Spotify
 gpg --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 931FF8E79F0876134EDDBDCCA87FF9DF48BF1C90
-
+gpg --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0x4E2C6E8793298290
 
 # Update each package folder
 cd $SCRIPT_DIR
