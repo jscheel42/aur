33f14bb5ef7e03dbd385bd92c9e6b07da2f003fe
Joshua Scheel <4970360+jscheel42@users.noreply.github.com>
2021-08-08 18:35:06 -0700

    add chrome

--- update_all.sh
+++ update_all.sh
@@ -2,16 +2,15 @@
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-# Update the main repo (not submodules)
-git pull
 
-# Import GPG Keys
-# Spotify
-gpg --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 931FF8E79F0876134EDDBDCCA87FF9DF48BF1C90
-gpg --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0x4E2C6E8793298290
+# # Import GPG Keys
+# # Spotify
+# gpg --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 931FF8E79F0876134EDDBDCCA87FF9DF48BF1C90
+# gpg --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0x4E2C6E8793298290
 
 # Update each package folder
 cd $SCRIPT_DIR
+git pull
 for d in */; do
     $SCRIPT_DIR/update.sh $d
 done
